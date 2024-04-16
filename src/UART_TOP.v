f73395345f07a2a4858e0cc52cde3c65cd33ca51
Muhammad Saim Iqbal <76736982+m-saim-iqbal@users.noreply.github.com>
2024-04-16 23:23:09 +0500

    Update UART_TOP.v
    added all the necessary ports

--- src/UART_TOP.v
+++ src/UART_TOP.v
@@ -5,8 +5,12 @@
 
 module tt_um_UART (
     input ena, clk, rst_n, Transmit_btn,
-    input [7:0] data,
-    
+    // input [7:0] data,
+    input  logic [7:0] ui_in,    // Dedicated inputs - connected to the input switches
+    output logic [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
+    input  logic [7:0] uio_in,   // IOs: Bidirectional Input path
+    output logic [7:0] uio_out,  // IOs: Bidirectional Output path
+    output logic [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
     output TxD/*, TxD_Debug, rst_n_Debug, Transmit_btn_Debug, clk_Debug,*/
 //    output [7:0] anode, ssd
 );
@@ -17,7 +21,7 @@ module tt_um_UART (
         .clk(clk), 
         .rst_n(rst_n), 
         .Transmit(Transmit_out/* Transmit_btn*/),
-        .data(data),
+        .data(ui_in),
         
         .TxD(TxD)
         );
