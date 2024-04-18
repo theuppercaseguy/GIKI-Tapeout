03b11691f6807fd1835d97aeab9e34abb14b206e
Muhammad Saim Iqbal <76736982+m-saim-iqbal@users.noreply.github.com>
2024-04-18 10:46:59 +0500

    Update Debouncer.v

--- src/Debouncer.v
+++ src/Debouncer.v
@@ -19,16 +19,15 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
 module debouncer_clock(
     input i_Clk, 
     input i_Switch, 
     output o_Switch
     );
     
-      parameter c_DEBOUNCE_LIMIT = /*250000*/ 1000000;  // 10 ms at 25 MHz
+      parameter c_DEBOUNCE_LIMIT = /*250000*/ 100000;  // 10 ms at 25 MHz
        
-      reg [20:0] r_Count = 0;
+      reg [27:0] r_Count = 0;
       reg r_State = 1'b0;
      
       always @(posedge i_Clk)
@@ -39,7 +38,7 @@ module debouncer_clock(
           r_Count <= r_Count + 1;
      
         // End of counter reached, switch is stable, register it, reset counter
-        else if (r_Count == c_DEBOUNCE_LIMIT)
+        else if (r_Count >= c_DEBOUNCE_LIMIT)
         begin
           r_State <= i_Switch;
           r_Count <= 0;
