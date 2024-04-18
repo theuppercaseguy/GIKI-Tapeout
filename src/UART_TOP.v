bdd1f492c89035caf36a42811f8b6486819ce8b4
Muhammad Saim Iqbal <76736982+m-saim-iqbal@users.noreply.github.com>
2024-04-18 10:45:10 +0500

    Update UART_TOP.v

--- src/UART_TOP.v
+++ src/UART_TOP.v
@@ -1,8 +1,3 @@
-
-
-
-
-
 module tt_um_UART (
     // input [7:0] data,
     
@@ -18,47 +13,66 @@ module tt_um_UART (
 //    output [7:0] anode, ssd
 );
 
-    wire Transmit_out;
+    wire Transmit_out;   
+    //transmit_btn = uio_in[0]
+    //transmit_switch = uio_in[1]
+    ///TxD = uio_out[7]
 
     assign uo_out = 8'b11111111;
     assign uio_out[6:0] = 7'b1111111;
     assign uio_oe = 8'b11111111;
-    
+
+ 
     Transmitter T1(
         .clk(clk), 
         .rst_n(rst_n), 
-        .Transmit(uio_in[0]/* Transmit_btn*/),
+        .Transmit( (Transmit_out | uio_in[1])/* Transmit_btn*/),
         .data(ui_in),
-        
         .TxD(uio_out[7])
         );
         
-    // debouncer_clock DB(
-    //         .i_Clk(clk), 
-    //         .i_Switch(Transmit_btn), 
-    //         .o_Switch(Transmit_out)
-    //         );
+    debouncer_clock DB(
+            .i_Clk(clk), 
+            .i_Switch(uio_in[0]), 
+            .o_Switch(Transmit_out)
+            );
             
-//   SSD_Display(
-//            .clk(clk), 
-//            .rst(rst),
-//            .anode(anode), 
-//            .ssd(ssd), 
-//            .data_in(data)
-//            );
-   
-   
-/*   assign TxD_Debug = TxD;
-   assign rst_n_Debug = rst;
-   assign Transmit_btn_Debug = Transmit_btn;
-   assign clk_Debug = clk;
-   */
-   
-   
+            
+    //reciver module instentiation
     
-
+    // Clock frequency in hertz.
+    parameter CLK_HZ = 100000000; //need to be changed to 50mhz
+    parameter BIT_RATE =   9600;
+    parameter PAYLOAD_BITS = 8;
+    
+    wire [PAYLOAD_BITS-1:0]  uart_rx_data;
+    wire        uart_rx_valid;
+    wire        uart_rx_break;
+    
+    reg  [PAYLOAD_BITS-1:0]  led_reg;
+    assign      rx_leds = led_reg;
+    
+    
+    always @(posedge clk) begin
+        if(!rst_n) begin
+            led_reg <= 8'hF0;
+        end else if(uart_rx_valid) begin
+            led_reg <= uart_rx_data[7:0];
+        end
+    end
+    
+        receiver #(
+            .BIT_RATE(BIT_RATE),
+            .PAYLOAD_BITS(PAYLOAD_BITS),
+            .CLK_HZ  (CLK_HZ  )
+        ) i_uart_rx(
+            .clk          (clk        ), // Top level system clock input.
+            .resetn       (rst_n         ), // Asynchronous active low reset.
+            .uart_rxd     (uart_rxd      ), // UART Recieve pin.
+            .uart_rx_en   (1'b1          ), // Recieve enable
+            .uart_rx_break(uart_rx_break ), // Did we get a BREAK message?
+            .uart_rx_valid(uart_rx_valid ), // Valid data recieved and available.
+            .uart_rx_data (uart_rx_data  )  // The recieved data.
+    );
+   
 endmodule
-
-
-
-
