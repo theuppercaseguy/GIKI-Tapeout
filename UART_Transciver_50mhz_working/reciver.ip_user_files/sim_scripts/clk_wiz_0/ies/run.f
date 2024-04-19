85e1a5004fb91fb37953abb8f0ccadbdddb3e6ac
theuppercaseguy <saadan06@gmail.com>
2024-04-19 14:23:34 +0500

    testing created

--- UART_Transciver_50mhz_working/reciver.ip_user_files/sim_scripts/clk_wiz_0/ies/run.f
+++
@@ -1,8 +0,0 @@
--makelib ies_lib/xil_defaultlib \
-  "../../../../reciver.srcs/sources_1/ip/clk_wiz_0/clk_wiz_0_clk_wiz.v" \
-  "../../../../reciver.srcs/sources_1/ip/clk_wiz_0/clk_wiz_0.v" \
--endlib
--makelib ies_lib/xil_defaultlib \
-  glbl.v
--endlib
-
